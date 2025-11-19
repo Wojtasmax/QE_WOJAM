f3e25bfce0bcead9d05e3986c442d9b5eab1ea3d
VVoit8282 <wojciech.artur.nowacki@gmail.com>
2025-11-19 23:58:43 +0100

    4 (c)(d)

--- ProblemSet1/TASK_4.jl
+++ ProblemSet1/TASK_4.jl
@@ -5,7 +5,7 @@ using Statistics
 using Random
 using Distributions
 using LinearAlgebra
-
+using IterativeSolvers
 #1. load data into a df
 cd(@__DIR__)
 df = CSV.read("asset_returns.csv", DataFrame)
@@ -28,8 +28,14 @@ A = vcat(
 b=vcat(zeros(size(cov_matrix, 1)), μ, 1.0)
 
 # Report the condition number of the A matrix
-
 cond_A = cond(A)
+
+# custom function for checking if conditions are satisfied
+function sanity_check(weights)
+    sum_weights = sum(weights)
+    portfolio_return = dot(weights, mean_returns)
+    return sum_weights, portfolio_return
+end 
 
 # Solve the system using the following methods:
 
@@ -39,10 +45,45 @@ result1 = A\b
 weights1 = result1[1:end-2]
 
 # sanity check czy coś
-weights1'*ones(length(weights1))
-weights1'*mean_returns
+sanity_check(weights1)
 
-# 2: 
+# 2 Jacobi or Gauss-Seidel: 
 
+gramA = A'*A
+#check for zeros on diagonal
+any(diag(gramA) .== 0)
+#false so there aint no zeros, great
+cond_gramA = cond(gramA)
+# TODO check diagonal dominance (by columns)
+function check_diag_dominance(Mat::Matrix)
+    # 1- diag dominant 0- guess what
+    for i in axes(Mat, 1)
+        if 2*Mat[i, i]<sum(Mat[:,i])
+            return 0
+        end
+    end
+    return 1
+end
 
+check_diag_dominance(gramA)
 
+# It seems that the A^T A matrix is not diagonally dominant (it doesnt look so anyway)
+# So there is no need to write this method indeed the custom one returns nonsense
+# TODO write custom, for now I use the built in one
+result2=jacobi(gramA, A'*b)
+weights2=result2[1:end-2]
+sanity_check(weights2)
+
+# 3 Conjugate Gradient method (CG) 
+issymmetric(gramA)
+isposdef(gramA)
+result3=cg(gramA, A'*b)
+weights3=result3[1:end-2]
+sanity_check(weights3)
+
+# 4 GMRES (the generalized minimal residual method)
+result4=gmres(A, b)
+weights4=result4[1:end-2]
+sanity_check(weights4)
+
+# 5 sleep
