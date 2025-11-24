3539bb88373da5e2c747623f6273d41cb6dddadc
Aetolian02 <j.muchorowsk@student.uw.edu.pl>
2025-11-24 23:40:34 +0100

    Solving problem with domain error

--- ProblemSet1/TASK_3.jl
+++ ProblemSet1/TASK_3.jl
@@ -15,7 +15,6 @@ consumption_people1_good2(p_1; p_2, x, σ, ω_1, ω_2) = (( (1 - x)^σ * p_2 ^ (
 consumption_people2_good1(p_1; p_2, x, σ, ω_1, ω_2) = (( x^σ * p_1 ^ (-σ) ) * (p_1 * ω_2[1] + p_2 * ω_2[2]))/(x^σ * p_1^(1 - σ) + (1 - x)^σ * p_2 ^(1 - σ))
 consumption_people2_good2(p_1; p_2, x, σ, ω_1, ω_2) = (( (1 - x)^σ * p_2 ^ (-σ) ) * (p_1 * ω_2[1] + p_2 * ω_2[2]))/(x^σ * p_1^(1 - σ) + (1 - x)^σ * p_2 ^(1 - σ))
 
-#domain error - needs to be fixed
 function solve(price, share, elasticity, wealth1, wealth2) 
     
     #consumption_people 1 - please check for mistakes
@@ -26,8 +25,8 @@ function solve(price, share, elasticity, wealth1, wealth2)
     consumption_people2_good1(p_1; p_2, x, σ, ω_1, ω_2) = (( x^σ * p_1 ^ (-σ) ) * (p_1 * ω_2[1] + p_2 * ω_2[2]))/(x^σ * p_1^(1 - σ) + (1 - x)^σ * p_2 ^(1 - σ))
     consumption_people2_good2(p_1; p_2, x, σ, ω_1, ω_2) = (( (1 - x)^σ * p_2 ^ (-σ) ) * (p_1 * ω_2[1] + p_2 * ω_2[2]))/(x^σ * p_1^(1 - σ) + (1 - x)^σ * p_2 ^(1 - σ))
 
-    equation(p_1) =  consumption_people1_good1(p_1; p_2 = price, x = share,σ = elasticity, ω_1 = wealth1, ω_2 = wealth2) + consumption_people2_good1(p_1; p_2 = price, x = share,σ = elasticity, ω_1 = wealth1, ω_2 = wealth2 ) - (wealth1[1] + wealth2[2])
-    solution = find_zero(equation, 1.2, verbose = true)
+    equation(p_1) =  consumption_people1_good1(p_1; p_2 = price, x = share,σ = elasticity, ω_1 = wealth1, ω_2 = wealth2) + consumption_people2_good1(p_1; p_2 = price, x = share,σ = elasticity, ω_1 = wealth1, ω_2 = wealth2 ) - (wealth1[1] + wealth2[1])
+    solution = find_zero(equation, (0.0, Inf))
     return solution
 end 
 
