5a033d1d3d003e7e174e1681b27448fdf0c0f800
Aetolian02 <j.muchorowsk@student.uw.edu.pl>
2025-11-25 14:08:46 +0100

    Adding better solutions to Task3

--- ProblemSet1/TASK_3.jl
+++ ProblemSet1/TASK_3.jl
@@ -23,8 +23,9 @@ function solve(price, share, elasticity, wealth1, wealth2)
     consumption_people2_good2(p_1; p_2, x, σ, ω_1, ω_2) = (( (1 - x)^σ * p_2 ^ (-σ) ) * (p_1 * ω_2[1] + p_2 * ω_2[2]))/(x^σ * p_1^(1 - σ) + (1 - x)^σ * p_2 ^(1 - σ))
 
     equation(p_1) =  consumption_people1_good1(p_1; p_2 = price, x = share,σ = elasticity, ω_1 = wealth1, ω_2 = wealth2) + consumption_people2_good1(p_1; p_2 = price, x = share,σ = elasticity, ω_1 = wealth1, ω_2 = wealth2 ) - (wealth1[1] + wealth2[1])
-    solution = find_zero(equation, (0.0, Inf))
-    return solution
+    equation_z(z) = equation(exp(z))
+    zstar = find_zero(equation_z, 0.0; autodiff = :forward)
+    return exp(zstar)
 end 
 
 #Choosing the range of x
@@ -48,12 +49,13 @@ plot(shares, solutions_consumption, label = "Consumption of good 1 by person 1")
 plot!(shares, ω_1[1]+ω_2[1].-solutions_consumption, label = "Consumption of good 1 by person 2")
 
 #Changing the elasticity
-σ = 0.2
+σ_2 = 1.52
 
-solutions_price = [
-    solve(p_2, x, σ , ω_1, ω_2)
+solutions_price_2 = [
+    solve(p_2, x, σ_2, ω_1, ω_2)
     for x in shares
 ]
 
 #Ploting the solutions
 plot(shares, solutions_price)
+plot!(shares, solutions_price_2)
