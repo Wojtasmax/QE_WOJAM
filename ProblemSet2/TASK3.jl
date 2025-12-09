4fa9b52551e6dbab9af88b37bdf7eac073ce25b8
Aetolian02 <j.muchorowsk@student.uw.edu.pl>
2025-12-09 18:43:21 +0100

    Adding Task 3

--- ProblemSet2/TASK3.jl
+++ ProblemSet2/TASK3.jl
@@ -13,17 +13,17 @@ function model_parameters(γ;
     return (; γ, A, δ, β, α, khat, chat, k0)
 end
 
-function transition_equation(x, T, parameters)
+function transition_equation(res, x, T, parameters)
 
-    A  = parameters[:A]
-    δ  = parameters[:δ]
-    β  = parameters[:β]
-    α  = parameters[:α]
-    γ  = parameters[:γ]
-    k0 = parameters[:k0]
-    chat = params[:chat]
+    A  = parameters.A
+    δ  = parameters.δ
+    β  = parameters.β
+    α  = parameters.α
+    γ  = parameters.γ
+    k0 = parameters.k0
+    chat = parameters.chat
 
-    res = Zeros(2*T+1) # vector for residuals
+    res = zeros(2*T+1) # vector for residuals
 
     for t in 1:T
         res[t] = x[t]^(-γ) - (β*x[t+1]^(-γ) * (α*A*x[T+t+1]^(α-1) + 1 - δ)) # difference between consumption (in fact - the inverse of utility) from Euler equation and our data
@@ -40,34 +40,25 @@ end
 
 function solve_transition_path(T, parameters)
 
-    A  = parameters[:A]
-    δ  = parameters[:δ]
-    β  = parameters[:β]
-    α  = parameters[:α]
-    γ  = parameters[:γ]
-    k0 = parameters[:k_begin]
-    chat = params[:chat]
-    khat = params[:khat]
-
-    params = (γ, A, δ, β, α, k0, chat)
+    chat = parameters.chat
+    khat = parameters.khat
+    k0 = parameters.k0
 
     c_guess = fill(chat, T+1)
     k_guess = range(k0, khat, length=T+1)[2:end]
-    x0 = (c_guess, k_guess)
+    x0 = vcat(c_guess, k_guess)
 
-    sol = nlsolve(
-        (res, x) -> transition_equation!(res, x, T, params),
-        x0,
-        xtol=1e-10, ftol=1e-10, method=:newton
-    )
+    sol = nlsolve((res, x) -> transition_equation(res, x, T, parameters), x0,  method=:newton)
 
     if sol.converged
-        println("✔ Converged")
+        println("Converged")
+    else 
+        println("Not converged")
     end
 
     return sol
 end
 
 
-model_parameters(0.5)
-solve_transition_path(100, model_parameters)
+my_model = model_parameters(0.5)
+solve_transition_path(200, my_model)
