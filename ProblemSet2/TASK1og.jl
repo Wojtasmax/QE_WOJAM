851f00cd0ff353762923a2f6da6765d2f42627d8
VVoit8282 <wojciech.artur.nowacki@gmail.com>
2025-12-10 23:48:12 +0100

    al jfe

--- ProblemSet2/TASK1og.jl
+++ ProblemSet2/TASK1og.jl
@@ -1,7 +1,12 @@
-using QuadGK, Distributions, Roots, Plots
+using QuadGK, Distributions, Roots, Plots, NLsolve
 function foc_integral(ω, W, Rf, γ, μ, σ)
-    return quadgk(r -> (r-Rf)*((W*(ω*r+(1-ω)*Rf))^(-γ))*pdf(LogNormal(μ, σ), r), 0, Inf)[1]
+try
+    return quadgk(r -> (r-Rf)*(W*(ω*r+(1-ω)*Rf))^(-γ)*pdf(LogNormal(μ, σ), r), 0, Inf)[1]
+catch e
+    return NaN
 end
+end
+foc_integral(10, 1, 1.02, 2.0, 0.05, 0.1)
 # test it for γ=0
 # first naive check:
 function expected_result(μ, σ, Rf)
@@ -24,6 +29,17 @@ function optimal_portfolio(W, Rf, γ, μ, σ)
     ω_star=find_zero(f, 0.5, Order1())
     return ω_star
 end
+function optimal_portfolio(W, Rf, γ, μ, σ)
+    if abs(γ)<1
+        return 1.
+    end
+    function f!(F, x)
+        ω = x[1]
+        F[1] = foc_integral(ω, W, Rf, γ, μ, σ)
+    end
+    initial_guess = [0.5]
+    return nlsolve(f!, initial_guess).zero[1]
+end
 # test it for some parameters
 optimal_portfolio(1, 1.02, 3.0, 0.05, 0.1)
 # 1.5
@@ -31,4 +47,4 @@ const_optimal_portfolio(γ) = optimal_portfolio(1, 1.02, γ, 0.05, 0.1)
 plot(const_optimal_portfolio, .1, 10.0, xlabel="Risk Aversion γ", ylabel="Optimal Share in Risky Asset ω*",
     title="Optimal Portfolio Share vs Risk Aversion", legend=false)
 # the chart cuts at around 3. thats when the optimal share is above 1
-const_optimal_portfolio(4)
+const_optimal_portfolio(2)
