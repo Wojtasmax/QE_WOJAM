f1515b717f4269daab7edaf5b43b517d734b6437
VVoit8282 <wojciech.artur.nowacki@gmail.com>
2025-12-10 21:46:28 +0100

    task 1 fn vs

--- ProblemSet2/TASK1og.jl
+++ ProblemSet2/TASK1og.jl
@@ -1,6 +1,6 @@
 using QuadGK, Distributions, Roots, Plots
 function foc_integral(ω, W, Rf, γ, μ, σ)
-    return quadgk(r -> (r-Rf)*(W*(ω*r+(1-ω)*Rf))^(-γ)*pdf(LogNormal(μ, σ), r), 0, Inf)[1]
+    return quadgk(r -> (r-Rf)*((W*(ω*r+(1-ω)*Rf))^(-γ))*pdf(LogNormal(μ, σ), r), 0, Inf)[1]
 end
 # test it for γ=0
 # first naive check:
@@ -31,3 +31,4 @@ const_optimal_portfolio(γ) = optimal_portfolio(1, 1.02, γ, 0.05, 0.1)
 plot(const_optimal_portfolio, .1, 10.0, xlabel="Risk Aversion γ", ylabel="Optimal Share in Risky Asset ω*",
     title="Optimal Portfolio Share vs Risk Aversion", legend=false)
 # the chart cuts at around 3. thats when the optimal share is above 1
+const_optimal_portfolio(4)
