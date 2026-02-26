2614dfad882680218f98ed3f65cc0cdc562a1b7c
oludew <o.ludew@student.uw.edu.pl>
2026-02-27 00:50:19 +0100

    zmiany

--- FinalProject/module.jl
+++ FinalProject/module.jl
@@ -6,12 +6,12 @@ export ProjectParams
 
 @with_kw struct ProjectParams
     #==== Parametry globalne ====#
-    α::Float64 = 0.3
-    v::Float64 = 0.6
-    r::Float64 = 0.04
-    δ::Float64 = 0.08
-    w::Float64 = 1.0
-    β::Float64 = 1.0 / (1.0 + r)
+    α= 0.3
+    v= 0.6
+    r= 0.04
+    δ= 0.08
+    w= 1.0
+    β = 1.0 / (1.0 + r)
     
     #==== Parametry procesu Markowa ====#
     ρ::Float64 = 0.9
@@ -19,14 +19,22 @@ export ProjectParams
     N_z::Int = 7  #to jest z czapy
 
     #==== Wyniki procesu Markowa ====#
-    z_vec::Vector{Float64}
-    P_z::Matrix{Float64}
-    λ_z::Vector{Float64}
+    z_vec::Vector{Float64}=Float64[]
+    P_z::Matrix{Float64}=zeros(1,1)
+    λ_z::Vector{Float64}=Float64[]
+
+
+    #===Adjustment Cost====#
+    #losowe wartosci    
+    γ = 0.05          
+    F = 0.01          #fixed cost
+    ps= 0.80         
 end
 
 
 function ProjectParams(; α=0.3, v=0.6, r=0.04, δ=0.08, w=1.0, ρ=0.9, σ_ε=0.12, N_z=7)
-    
+    @unpack α,v,r,δ,w,ρ,σ_ε,N_z
+
     β = 1.0 / (1.0 + r)
     
    
@@ -45,8 +53,39 @@ function ProjectParams(; α=0.3, v=0.6, r=0.04, δ=0.08, w=1.0, ρ=0.9, σ_ε=0.
     return ProjectParams(
         α=α, v=v, r=r, δ=δ, w=w, β=β, 
         ρ=ρ, σ_ε=σ_ε, N_z=N_z, 
-        z_vec=z_vec, P_z=P_z, λ_z=λ_z
+        z_vec=z_vec, P_z=P_z, λ_z=λ_z,
+        γ=γ, F=F, ps=ps
     )
 end
-
+function ADJ_COST(model::ProjectParams,i,k, )
+    @unpack F,γ =model
+    if i !=0
+        return ((γ/2)*(i/k)^2)*k+F*k
+    else
+        return 0.0
+    end
 end
+
+
+function IRR(model::ProjectParams,i)
+    @unpack ps = model
+    if i >=0
+        return 1.0
+    else
+        return ps
+    end
+end
+
+
+function OPTIMAL_H(model::ProjectParams,k,z) #jak cos sie tu bedzie jebac to zmienić tak zeby ulamke w potedze byl dodatni
+    @unpack w,v,α = model
+    return (w/(v*z*k^α))^(1/(v-1))
+end
+
+
+function OPERATING_PROFIT(model::ProjectParams,k,z)  
+    @unpack α,v,w = model
+    h=OPTIMAL_H(model::ProjectParams,k)
+    return z*k^α*h^v-w*h
+end
+end #moduł
